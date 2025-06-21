fa8a8c9b6d8c8f871b246658e8bdf7f85623a435
Xeviozo <xeviozo@gmail.com>
2025-06-21 18:05:48 +0200

    Anotherpulltest

--- Character.gd
+++ Character.gd
@@ -1,12 +1,11 @@
 extends CharacterBody2D
-
 var Movedir: Vector2
 var movespeed = 320
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
+#test
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
